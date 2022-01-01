402882a1429ae1abb67a91fcc5192484beeab2b9
wagner <wagncarv@gmail.com>
2021-12-31 23:38:05 -0300

    translate errors, crudry, enum type

--- lib/food_diary_web/schema/types/root.ex
+++ lib/food_diary_web/schema/types/root.ex
@@ -1,7 +1,9 @@
 defmodule FoodDiaryWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
+  alias Crudry.Middlewares.TranslateErrors
   alias FoodDiaryWeb.Resolvers.User, as: UsersResolver
 
+  import_types FoodDiaryWeb.Schema.Types.Meal
   import_types FoodDiaryWeb.Schema.Types.User
 
   object :root_query do
@@ -18,6 +20,7 @@ defmodule FoodDiaryWeb.Schema.Types.Root do
       arg :input, non_null(:create_user_input)
 
       resolve &UsersResolver.create/2
+      middleware TranslateErrors
     end
 
     @desc "Deletes a user"
@@ -25,6 +28,7 @@ defmodule FoodDiaryWeb.Schema.Types.Root do
       arg :id, non_null(:id)
 
       resolve &UsersResolver.delete/2
+      middleware TranslateErrors
     end
   end
 end
